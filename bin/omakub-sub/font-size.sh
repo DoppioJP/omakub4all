40247d6776c24442264437f7fc6e88862839307e
DoppioJP <jakub@doppio.jp>
2025-04-28 17:25:38 +0200

    Allow for more font sizes

--- bin/omakub-sub/font-size.sh
+++ bin/omakub-sub/font-size.sh
@@ -1,4 +1,4 @@
-choice=$(gum choose {7..14} "<< Back" --height 11 --header "Choose your terminal font size")
+choice=$(gum choose {7..18} "<< Back" --height 11 --header "Choose your terminal font size")
 
 if [[ $choice =~ ^[0-9]+$ ]]; then
 	sed -i '' "s/^size = .*$/size = $choice/g" ~/.config/alacritty/font-size.toml
