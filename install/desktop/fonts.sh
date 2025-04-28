d896225c3778d3d318f35d4814b30ae9a5b9c78f
DoppioJP <jakub@doppio.jp>
2025-04-28 06:37:43 +0200

    fix: fonts for macOS

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -1,15 +1,36 @@
-mkdir -p ~/.local/share/fonts
+#!/bin/bash
 
+# macOS font directory
+FONT_DIR="$HOME/Library/Fonts"
+echo "Installing fonts for macOS..."
+
+# Create font directory if it doesn't exist
+mkdir -p "$FONT_DIR"
+
+# Change to temporary directory
 cd /tmp
-wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
-unzip CascadiaMono.zip -d CascadiaFont
-cp CascadiaFont/*.ttf ~/.local/share/fonts
+
+# Download and install Cascadia Mono Nerd Font
+echo "Downloading Cascadia Mono Nerd Font..."
+curl -L -o CascadiaMono.zip https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
+
+echo "Extracting Cascadia Mono Nerd Font..."
+unzip -q CascadiaMono.zip -d CascadiaFont
+echo "Installing Cascadia Mono Nerd Font..."
+cp CascadiaFont/*.ttf "$FONT_DIR"
 rm -rf CascadiaMono.zip CascadiaFont
 
-wget -O iafonts.zip https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.zip
-unzip iafonts.zip -d iaFonts
-cp iaFonts/iA-Fonts-master/iA\ Writer\ Mono/Static/iAWriterMonoS-*.ttf ~/.local/share/fonts
+# Download and install iA Writer Mono font
+echo "Downloading iA Writer Mono font..."
+curl -L -o iafonts.zip https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.zip
+
+echo "Extracting iA Writer Mono font..."
+unzip -q iafonts.zip -d iaFonts
+echo "Installing iA Writer Mono font..."
+cp iaFonts/iA-Fonts-master/iA\ Writer\ Mono/Static/iAWriterMonoS-*.ttf "$FONT_DIR"
 rm -rf iafonts.zip iaFonts
 
-fc-cache
+# Return to original directory
 cd -
+
+echo "Font installation complete!"
